8068674c66876e1c3e6cbd0a55aac8e175b98128
emrahburak <gemrahburak@gmail.com>
2025-06-24 18:00:05 +0300

    UPDATED --test/downsub_post.sh  --test/dowsub_get.sh

--- test/downsub_post.sh
+++ test/downsub_post.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-# Kullanım: ./downsub_request.sh <API_URL> <api-type: local|remote>
+# Kullanım: ./downsub_post.sh <API_URL> <YOUTUBE_URL> <SUB_LANG>
 
 API_URL="$1"
-API_TYPE="$2"
+VIDEO_URL="$2"
+SUB_LANG="$3"
 
-# Gömülü sabit değerler
-# URL="https://www.youtube.com/watch?v=XTsaZWzVJ4c&t=1s"
-URL="https://www.youtube.com/watch?v=va3kJ1YBBXo"
-SUB_LANG="en"
-
-# Kontroller
-if [ -z "$API_URL" ] || [ -z "$API_TYPE" ]; then
-  echo "Kullanım: $0 <API_URL> <api-type: local|remote>"
+if [ -z "$API_URL" ] || [ -z "$VIDEO_URL" ] || [ -z "$SUB_LANG" ]; then
+  echo "Kullanım: $0 <API_URL> <YOUTUBE_URL> <SUB_LANG>"
   exit 1
 fi
 
-if [ "$API_TYPE" != "local" ] && [ "$API_TYPE" != "remote" ]; then
-  echo "Hatalı api-type: '$API_TYPE'. Sadece 'local' veya 'remote' olabilir."
-  exit 1
-fi
+FULL_URL="${API_URL}/downsub"
 
 echo "İstek gönderiliyor:"
 echo "API URL   : $API_URL"
-echo "API Tipi  : $API_TYPE"
-echo "Video URL : $URL"
+echo "Video URL : $VIDEO_URL"
 echo "Dil Kodu  : $SUB_LANG"
 
 # İstek gönder
-curl -X POST "$API_URL" \
+curl -X POST "$FULL_URL" \
   -H "Content-Type: application/json" \
   -H "x-service: downsub-api" \
-  -d "{\"url\": \"$URL\", \"sub_lang\": \"$SUB_LANG\"}"
+  -d "{\"url\": \"$VIDEO_URL\", \"sub_lang\": \"$SUB_LANG\"}"
 
